165c85a267cbc8c8bab49ccca73d0e701e52f856
Tiago Costa <tiago.costa@pt.glory-global.com>
2022-10-22 21:48:55 +0100

    ping atv

--- src/main.go
+++ src/main.go
@@ -6,10 +6,11 @@ import (
 	"time"
 
 	"github.com/go-co-op/gocron"
+	"github.com/go-ping/ping"
 	"github.com/tiagordc/go-apt-reboot/src/tplink"
 )
 
-func run() {
+func restart() {
 
 	address := os.Getenv("TAPO_IP")
 	user := os.Getenv("TAPO_USERNAME")
@@ -17,35 +18,64 @@ func run() {
 	plug := tplink.New(address, user, password)
 
 	if err := plug.Handshake(); err != nil {
-		log.Panic(err)
+		log.Println("Handshake error:", err)
+		return
 	}
 
 	if err := plug.Login(); err != nil {
-		log.Panic(err)
+		log.Println("Login error:", err)
+		return
 	}
 
 	info, err := plug.GetDeviceInfo()
 
 	if err != nil {
-		log.Panic(err)
+		log.Println("Could not get plug data:", err)
+		return
 	}
 
 	if info.Result.DeviceON {
 		log.Println("Restarting")
-		plug.Switch(false)
-		time.Sleep(15 * time.Second)
-		plug.Switch(true)
+		if err := plug.Switch(false); err != nil {
+			log.Println("Could not switch off:", err)
+			return
+		}
+		time.Sleep(5 * time.Second)
 	} else {
-		log.Println("Turning on")
-		plug.Switch(true)
+		log.Println("Starting")
+	}
+
+	if err := plug.Switch(true); err != nil {
+		log.Println("Could not switch on:", err)
 	}
 
 }
 
+func isOnline(address string) bool {
+	pinger, err := ping.NewPinger(address)
+	if err != nil {
+		log.Println("Ping error:", err)
+		return false
+	}
+	pinger.Count = 1
+	pinger.Timeout = 1 * time.Second
+	pinger.Run()
+	stats := pinger.Statistics()
+	return stats.PacketsSent == stats.PacketsRecv
+}
+
 func main() {
-	cron := os.Getenv("CRON")
 	s := gocron.NewScheduler(time.UTC)
-	s.Cron(cron).Do(run)
+	atv := os.Getenv("ATV_ADDRESS")
+	cron, present := os.LookupEnv("CRON")
+	if present {
+		s.Cron(cron).Do(restart)
+	}
+	s.Every(10).Seconds().Do(func() {
+		if !isOnline(atv) {
+			restart()
+		}
+	})
 	s.StartAsync()
 	s.StartBlocking()
 }
